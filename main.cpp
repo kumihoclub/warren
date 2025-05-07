535ae2ca1732f35d142dac65d5ebe7f4168716fe
Rebecca <github@kumiho.club>
2025-05-07 14:13:58 -0700

    Re-embedded sqlite. Updated build files.

--- main.cpp
+++ main.cpp
@@ -8,52 +8,21 @@
 
 #include "squirrel.h"
 
-#include "leveldb/db.h"
+#include "sqlite3.h"
 
 static HSQUIRRELVM v = nullptr;
 
-static leveldb::DB* db = nullptr;
-static leveldb::Options options;
-
 static SDL_Window *window = NULL;
 static SDL_Renderer *renderer = NULL;
-//static SDL_AudioDeviceID audio_device = 0;
+
+static sqlite3* db = nullptr;
 
 SDL_AppResult SDL_AppInit(void** appstate, int argc, char* argv[]) {
 
     SDL_SetAppMetadata("Warren Test", "1.0", "com.warrentest");
 
-    options.create_if_missing = true;
-    leveldb::Status status = leveldb::DB::Open(options, "./testdb", &db);
-    if (!status.ok()) {
-        SDL_Log("Couldn't initialize LevelDB: %s", status.ToString());
-        return SDL_APP_FAILURE;
-    }
-
-    // Add 256 values to the database
-    leveldb::WriteOptions writeOptions;
-    for (unsigned int i = 0; i < 256; ++i)
-    {
-        std::ostringstream keyStream;
-        keyStream << "Key" << i;
-
-        std::ostringstream valueStream;
-        valueStream << "Test data value: " << i;
-
-        db->Put(writeOptions, keyStream.str(), valueStream.str());
-    }
-
-    // Iterate over each item in the database and print them
-    leveldb::Iterator* it = db->NewIterator(leveldb::ReadOptions());
-
-    for (it->SeekToFirst(); it->Valid(); it->Next())
-    {
-        std::cout << it->key().ToString() << " : " << it->value().ToString() << std::endl;
-    }
-
-    if (false == it->status().ok())
-    {
-        SDL_Log("An error was found during LevelDB scan: %s", it->status().ToString());
+    if (sqlite3_open_v2("test", &db, SQLITE_OPEN_READWRITE|SQLITE_OPEN_CREATE|SQLITE_OPEN_MEMORY|SQLITE_OPEN_NOMUTEX, nullptr) != SQLITE_OK) {
+        SDL_Log("Couldn't initialize sqlite: %s",sqlite3_errmsg(db));
         return SDL_APP_FAILURE;
     }
 
@@ -92,6 +61,6 @@ SDL_AppResult SDL_AppIterate(void *appstate){
 }
 
 void SDL_AppQuit(void *appstate, SDL_AppResult result) {
-    if (v) { sq_close(v); }
-    if (db) {delete db; db = nullptr;}
+    if (v) { sq_close(v); v = nullptr;}
+    if (db) {sqlite3_close(db); db=nullptr;}
 }
